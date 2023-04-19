39eb9a07f9f3e163dd57d6d36e90028d7bdd6237
Andryss <andr.krivosheev@gmail.com>
2023-04-19 12:07:24 +0300

    Add printing result

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 
-import numpy as np
-
+from approximators import *
 from functions import *
 
 
@@ -24,13 +23,14 @@ def read_point_from_console() -> [float, float]:
 
 
 def read_table_function_from_console() -> TableFunction:
-    points_count = read_int_from_console("points count")
+    points_count = read_int_from_console("points count (at least 5)")
+    assert points_count >= 5, "Must be at least 5 points"
     print("\nEnter points coordinates sequentially (in each row like \"<x> <y>\"):")
     points = []
     for i in range(points_count):
         points.append(read_point_from_console())
     try:
-        return TableFunction(pd.DataFrame(data=np.array(points), columns=['x','y']))
+        return TableFunction(pd.DataFrame(data=np.array(points), columns=['x', 'y']))
     except Exception as e:
         raise Exception(f"Can't read table function from console: {e.__str__()}")
 
@@ -41,6 +41,7 @@ def read_table_function_from_file() -> TableFunction:
     try:
         values = pd.read_csv(filename, header=None).values
         table = pd.DataFrame(data=values, columns=['x', 'y'])
+        assert len(table) >= 5, "Must be at least 5 points"
         return TableFunction(table)
     except Exception as e:
         raise Exception("file \"" + filename + "\" can't be opened: " + e.__str__())
@@ -59,10 +60,35 @@ def read_table_function() -> TableFunction:
         raise Exception("No such option :(")
 
 
+def print_result_entity(result_entity: ApproximationResultEntity):
+    print(f"\n{result_entity.approximator.name}:")
+    if isinstance(result_entity, ApproximationResultEntityError):
+        print(f"error: {result_entity.error.__str__()}")
+    else:
+        print(f"function: {result_entity.approximated_function.__str__()}")
+        print(f"deviation: {result_entity.metrics.standard_deviation}")
+
+
+def print_result(result: ApproximationResult):
+    # pd.set_option('display.expand_frame_repr', False)
+
+    print("\nHere is approximation result:")
+    print(f"Source function was: \n{result.source_function.table().T}")
+    for result_entity in result.approximations:
+        print_result_entity(result_entity)
+
+
+def show_result(result: ApproximationResult):
+    print_result(result)
+
+
 def run():
     try:
         table_function = read_table_function()
+        # table_function = TableFunction(pd.DataFrame(data=pd.read_csv("table_1.csv", header=None).values, columns=['x', 'y']))
         print(table_function.table())
+        approximation_result = approximate(table_function)
+        show_result(approximation_result)
     except Exception as e:
         print(e, file=sys.stderr)
 
