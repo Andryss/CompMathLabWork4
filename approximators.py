ba08b06429b3c55635b68bda3d727a42100e3cd1
Andryss <andr.krivosheev@gmail.com>
2023-04-19 09:47:00 +0300

    Add approximate function

--- approximators.py
+++ approximators.py
@@ -21,9 +21,11 @@ class VarianceCalculator:
 
     @staticmethod
     def standard_deviation_from_squared_deviation(s: float, n: int) -> float:
-        val = s / n
-        assert val > 0, "Can't calculate standard deviation"
-        return math.sqrt(val)
+        try:
+            val = s / n
+            return math.sqrt(val)
+        except Exception as e:
+            raise Exception(f"Can't calculate standard_deviation: {e.__str__()}")
 
     @staticmethod
     def squared_deviation_and_standard_deviation(table_function: TableFunction,
@@ -45,13 +47,13 @@ class _PolynomialSystemGenerator:
     @staticmethod
     def generate(table_function: TableFunction, n: int) -> np.ndarray:
         system = [[] for _ in range(n)]
-        x_vals = table_function.x_values()
+        x_vals = table_function.x_values().copy()
         x = pd.Series([1] * len(x_vals))
         for bottom_row in range(-n + 1, n):
             for row in range(max(0, bottom_row), min(n, bottom_row + n)):   # window iteration
                 system[row].append(x.sum())
             x *= x_vals
-        y = table_function.y_values()
+        y = table_function.y_values().copy()
         for row in range(n):
             system[row].append(y.sum())
             y *= x_vals
@@ -116,7 +118,7 @@ class PowerApproximatorCoefficientResolver:
 
     @staticmethod
     def resolve(func: TableFunction) -> [float, float]:
-        assert all(func.table() > 0), "Can't resolve coefficients (both x and y must be positive)"
+        assert (func.table() > 0).all().all(), "Can't resolve coefficients (both x and y must be positive)"
         new_table = pd.DataFrame()
         new_table['x'] = func.table()['x'].apply(lambda x: math.log(x))
         new_table['y'] = func.table()['y'].apply(lambda y: math.log(y))
@@ -137,7 +139,7 @@ class ExponentialApproximatorCoefficientResolver:
 
     @staticmethod
     def resolve(func: TableFunction) -> [float, float]:
-        assert all(func.table()['y'] > 0), "Can't resolve coefficients (y must be positive)"
+        assert (func.table()['y'] > 0).all(), "Can't resolve coefficients (y must be positive)"
         new_table = pd.DataFrame()
         new_table['x'] = func.table()['x']
         new_table['y'] = func.table()['y'].apply(lambda y: math.log(y))
@@ -158,7 +160,7 @@ class LogarithmicApproximatorCoefficientResolver:
 
     @staticmethod
     def resolve(func: TableFunction) -> [float, float]:
-        assert all(func.table()['x'] > 0), "Can't resolve coefficients (x must be positive)"
+        assert (func.table()['x'] > 0).all(), "Can't resolve coefficients (x must be positive)"
         new_table = pd.DataFrame()
         new_table['x'] = func.table()['x'].apply(lambda x: math.log(x))
         new_table['y'] = func.table()['y']
@@ -173,3 +175,70 @@ class LogarithmicApproximator(Approximator):
         a, b = LogarithmicApproximatorCoefficientResolver.resolve(func)
         return Function(f'{a} * ln(x) + {b}',
                         lambda x: a * math.log(x) + b)
+
+
+def get_all_approximators() -> list[Approximator]:
+    return [
+        LinearApproximator(),
+        SquarePolynomialApproximator(),
+        CubePolynomialApproximator(),
+        PowerApproximator(),
+        ExponentialApproximator(),
+        LogarithmicApproximator()
+    ]
+
+
+class ApproximationMetrics:
+    squared_deviation: float
+    standard_deviation: float
+
+    def __init__(self, sq_d, st_d):
+        self.squared_deviation = sq_d
+        self.standard_deviation = st_d
+
+    @staticmethod
+    def from_approximated(table_function: TableFunction, approximated_function: Function):
+        return ApproximationMetrics(
+            *VarianceCalculator.squared_deviation_and_standard_deviation(table_function, approximated_function)
+        )
+
+
+class ApproximationResultEntity:
+    approximator: Approximator
+    approximated_function: Function
+    metrics: ApproximationMetrics
+
+    def __init__(self, app, app_f, met):
+        self.approximator = app
+        self.approximated_function = app_f
+        self.metrics = met
+
+
+class ApproximationResultEntityError(ApproximationResultEntity):
+    error: Exception
+
+    def __init__(self, app, err):
+        super().__init__(app, None, None)
+        self.approximator = app
+        self.error = err
+
+
+class ApproximationResult:
+    source_function: TableFunction
+    approximations: list[ApproximationResultEntity]
+
+    def __init__(self, src, app):
+        self.source_function = src
+        self.approximations = app
+
+
+def approximate(table_function: TableFunction) -> ApproximationResult:
+    approximation_results = []
+    for approximator in get_all_approximators():
+        try:
+            approximated_function = approximator.approximate(table_function)
+            metrics = ApproximationMetrics.from_approximated(table_function, approximated_function)
+            approximation_results.append(ApproximationResultEntity(approximator, approximated_function, metrics))
+        except Exception as e:
+            approximation_results.append(ApproximationResultEntityError(approximator, e))
+    return ApproximationResult(table_function, approximation_results)
